07d6e7dafdec468f745e2ba1f5c5daf9addb7c81
Herman group 12 <hd647@hotmail.com>
2015-03-14 16:37:58 -0700

    readding changes

--- backend/DataManager.js
+++ backend/DataManager.js
@@ -2,7 +2,7 @@
 var HashMap = require('hashmap').HashMap;
 var HashSet = require('./HashSet.js');
 var User = require('./User.js');
-var FbGraph = require('fbgraph');
+var FBGraph = require('fbgraph');
 
 var DataManager = function(){
 	//Instance variables:
@@ -19,6 +19,18 @@ DataManager.prototype.addUser = function(userid, token,name){
 		this.usersHashMap.get(userid).token = token;
 	}else{
 
+			var options = { //set for making an http request
+    		timeout:  3000
+  			, pool:     { maxSockets:  Infinity }
+  			, headers:  { connection:  "keep-alive" }
+		};
+
+		FBGraph.setAccessToken(token);
+
+		var friendslist = FBGraph.setOptions(options).get('/' + userid + '/friends', function(err, res) {
+    		console.log(res); 
+  		});
+  		
 		//1. create new userobject
 		newUser = this.createNewUser(userid, token, name);
 
