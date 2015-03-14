34968c6f4f2757a52df1fd1963581f29cb341300
Koushik Peri <w3p8@cs.ubc.ca>
2015-03-14 10:04:36 -0700

    Removed initializeGraph() and moved code to DataManager Constructor

--- backend/DataManager.js
+++ backend/DataManager.js
@@ -5,12 +5,10 @@ var DataManager = function(){
 	//Instance variables:
 	this.usersHashMap;	//HashMap userid -> user
 	this.pagesHashMap;	//HashMap pageid -> page
-};
-
-DataManager.prototype.initializeGraph = function(){
 	this.usersHashMap = new HashMap();
 	this.pagesHashMap = new HashMap();
 };
+
 
 DataManager.prototype.addUser = function(userid, token){
 	if(this.usersHashMap.has(userid)){
--- backend/main.js
+++ backend/main.js
@@ -4,5 +4,4 @@ var Router = require('./Router.js');
 
 //Initialization:
 var dataManager = new DataManager();
-dataManager.initializeGraph();
 var router = new Router(3000, dataManager);
