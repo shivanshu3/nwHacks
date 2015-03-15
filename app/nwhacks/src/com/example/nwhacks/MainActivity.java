4c65ac13909c6c927b0dea13e1570a291c9a64a0
Koushik Peri <w3p8@cs.ubc.ca>
2015-03-14 21:01:15 -0700

    List view on android

--- app/nwhacks/src/com/example/nwhacks/MainActivity.java
+++ app/nwhacks/src/com/example/nwhacks/MainActivity.java
@@ -5,14 +5,62 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+
 public class MainActivity extends Activity {
 
+	ListView list;
+    private List<String> List_file;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+        List_file =new ArrayList<String>();
+       list = (ListView)findViewById(R.id.list1);
+ 
+        CreateListView();
 	}
 
+	
+	private void CreateListView()
+    {
+         List_file.add("Alderan");
+         List_file.add("r2d2");
+         List_file.add("sing for the moment");
+         List_file.add("turn down for what");
+         List_file.add("nexus ");
+         List_file.add(" age of empires");
+         List_file.add(" hannibal");
+         List_file.add(" julius");
+         List_file.add(" market place iga");
+         List_file.add(" dominic republic");
+         List_file.add(" uggs");
+         List_file.add(" putin");
+         List_file.add(" marco polo");
+         List_file.add(" lalalalala");
+         List_file.add(" Queen Elizabeth");
+         List_file.add(" whoopi gouldberg");
+         
+         //Create an adapter for the listView and add the ArrayList to the adapter.
+         list.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,List_file));
+         list.setOnItemClickListener(new OnItemClickListener()
+           {
+                @Override
+                public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3)
+                {
+                    //args2 is the listViews Selected index
+                }
+           });
+    }
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
