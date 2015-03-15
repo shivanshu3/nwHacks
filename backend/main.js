d4ae06b3c33bcda049e528549e6224673e157f9f
Shivanshu Goyal <shivanshu3@gmail.com>
2015-03-14 17:03:50 -0700

    - Cleaned up main.js backend. - Front end prototype sends userid and token to the server.

--- backend/main.js
+++ backend/main.js
@@ -2,15 +2,6 @@
 var DataManager = require('./DataManager.js');
 var Router = require('./Router.js');
 
-var HashMap = require('hashmap').HashMap;
-
-
-var t = new HashMap();
-
 //Initialization:
 var dataManager = new DataManager();
-
-//we want to user info from the front end here:
-dataManager.addUser(10203105503070939,'CAACEdEose0cBANfuPbNluiwzFITD6GSZCVOPBjxX3ewRryk86aIYSXvcFG2NrYE0OaFE49rqLukYoqPswbEEXZAZBnyt7R7AMeqvWXNZAHtTP332Bl0itxmd9PPpcN2X2ALnpwHDTqti9rsE1FLwxDrP9bBsodn3EGYdz2VfBO6MIZAzVC5ufRMDN9hO73BspVt8fnF5XdbIdF90VTlW9C6ErJbgMmSAZD', "lol");
-
 var router = new Router(3000, dataManager);
