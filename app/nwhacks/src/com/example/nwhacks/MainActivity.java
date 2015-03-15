5bff22733840496dcf9894ad1ae4254ac54c2bc6
Emmalee <emmaleeroach@gmail.com>
2015-03-14 22:12:17 -0700

    crashing issue

--- app/nwhacks/src/com/example/nwhacks/MainActivity.java
+++ app/nwhacks/src/com/example/nwhacks/MainActivity.java
@@ -1,50 +1,43 @@
 package com.example.nwhacks;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import java.util.ArrayList;
-import java.util.List;
-import android.app.Activity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.AdapterView.OnItemClickListener;
-import com.facebook.*;
-import com.facebook.model.GraphObject;
-import com.facebook.model.GraphPlace;
+
 import com.facebook.model.GraphUser;
-import com.facebook.widget.*;
+import com.facebook.widget.LoginButton;
 
 public class MainActivity extends Activity {
 	
 	private LoginButton loginButton;
+    private GraphUser user;
 	ListView list;
     private List<String> List_file;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-        List_file =new ArrayList<String>();
-       list = (ListView)findViewById(R.id.list1);
- 
-        CreateListView();
         
-        loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
-            @Override
-            public void onUserInfoFetched(GraphUser user) {
-                MainActivity.this.user = user;
-                updateUI();
-                // It's possible that we were waiting for this.user to be populated in order to post a
-                // status update.
-                handlePendingAction();
-            }
-	}
+		  loginButton = (LoginButton) findViewById(R.id.login_button);
+	        loginButton.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
+	            @Override
+	            public void onUserInfoFetched(GraphUser user) {
+	                MainActivity.this.user = user;
+	                //updateUI();
+	                // It's possible that we were waiting for this.user to be populated in order to post a
+	                // status update.
+	                //handlePendingAction();
+	            }
+	        });
+        }
 
 	
 	private void CreateListView()
@@ -96,4 +89,6 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+
 }
